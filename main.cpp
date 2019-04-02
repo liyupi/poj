e6dc19d6b41deb68d35ffa08284d81cbf58cd4c7
liyupei <liyupei@bytedance.com>
2019-04-02 12:34:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,54 +1,31 @@
-// https://blog.csdn.net/huatian5/article/details/51519912
+// https://blog.csdn.net/lyy289065406/article/details/6642603
+// 条件：连续5个月的总额必亏损
+// ssddd ssddd ss
 #include <iostream>
-#include <string>
-
 
 using namespace std;
 
-int map[4][4] = {0};
-
-/**
- * 翻转
- * @param x
- * @param y
- */
-void flip(int x, int y) {
-    for (int i = 0; i < 4; ++i) {
-        map[i][y] = !map[i][y];
-    }
-    for (int j = 0; j < 4; ++j) {
-        map[x][j] = !map[x][j];
-    }
-    // 被翻了两次，翻回来
-    map[x][y] = !map[x][y];
-}
-
 int main() {
-    string str;
-    for (int i = 0; i < 4; ++i) {
-        cin >> str;
-        for (int j = 0; j < 4; ++j) {
-            if (str[j] == '+') {
-                flip(i, j);
-            }
+    int s, d;
+    while (cin >> s >> d) {
+        int sum = 0;
+        if (4 * s < d) {
+            sum = 10 * s - 2 * d;
+        } else if (3 * s < 2 * d) {
+            sum = 8 * s - 4 * d;
+        } else if (2 * s < 3 * d) {
+            sum = 6 * s - 6 * d;
+        } else if (s < 4 * d) {
+            sum = 3 * s - 9 * d;
+        } else {
+            sum = -1;
         }
-    }
-    int ans = 0;
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if (map[i][j]) {
-                ans++;
-            }
+        if (sum < 0) {
+            cout << "Deficit" << endl;
+        } else {
+            cout << sum << endl;
         }
-    }
-    cout << ans << endl;
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if (map[i][j]) {
-                cout << i + 1 << " " << j + 1 << endl;
-            }
-        }
-    }
 
+    }
     return 0;
 }

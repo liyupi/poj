277b9f2b61e16b73ba488f9d6fd6339479a729f0
liyupei <liyupei@bytedance.com>
2019-04-03 15:52:33 +0800

    add 2

--- main.cpp
+++ main.cpp
@@ -1,27 +1,63 @@
-// https://www.cnblogs.com/zzhzz/p/5837815.html
+// http://poj.org/problem?id=1573
 #include <iostream>
 
 using namespace std;
 
-int a[21] = {0};
+const int MAX = 15;
+char map[MAX][MAX];
+int visited[MAX][MAX];
 
 int main() {
-    int t;
-    while(cin >> t) {
-        while (t--) {
-            int n;
-            cin >> n;
-            for (int i = 1; i <= n; ++i) {
-                cin >> a[i];
-                int j = i - 1;
-                // 必须让左括号的个数和右括号匹配 i - j为右括号个数，a[i]-a[j]为左括号个数
-                while (a[i] - a[j] < i - j) {
-                    j--;
-                }
-                cout << i - j << " ";
-            }
-            cout << endl;
+    int m, n, x, y;
+    while (cin >> m >> n >> y) {
+        if (m == 0 && n == 0 && y == 0) {
+            return 0;
         }
-
+        x = 0;
+        y--;
+        memset(visited, -1, sizeof(visited));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> map[i][j];
+            }
+        }
+        int step = 0;
+        bool exit = false;
+        while (visited[x][y] == -1 && !exit) {
+            visited[x][y] = step++;
+            switch (map[x][y]) {
+                case 'W':
+                    y -= 1;
+                    if (y < 0) {
+                        exit = true;
+                    }
+                    break;
+                case 'N' :
+                    x -= 1;
+                    if (x < 0) {
+                        exit = true;
+                    }
+                    break;
+                case 'S':
+                    x += 1;
+                    if (x == m) {
+                        exit = true;
+                    }
+                    break;
+                case 'E':
+                    y += 1;
+                    if (y == n) {
+                        exit = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (exit) {
+            printf("%d step(s) to exit\n", step);
+        } else {
+            printf("%d step(s) before a loop of %d step(s)\n", visited[x][y], step - visited[x][y]);
+        }
     }
 }

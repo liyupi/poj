8a68e7cf0cde3f8a3d380eed92aeb50f73c83e49
liyupei <liyupei@bytedance.com>
2019-04-10 18:49:06 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,76 +1,63 @@
-// http://poj.org/problem?id=3349
-// 设计hash，记个素数99991
+// https://blog.csdn.net/liu940204/article/details/51254821
+// 设计hash，注意hash的设计不能太集中
 #include <iostream>
 
 using namespace std;
 
+int N, K;
+
 const int MAXN = 100001;
-const int BIG_PRIME = 99991;
+const int MAXK = 31;
+const int BIG_PRIME = 1000007;
 
-int snows[MAXN][6];
+int sums[MAXN][MAXK] = {0};
+int nums[MAXN][MAXK] = {0};
 
-int map[BIG_PRIME][5];
-
-int n;
+int hashTable[BIG_PRIME][10];
 
 int getHash(int i) {
     int sum = 0;
-    for (int j = 0; j < 6; ++j) {
-        sum += snows[i][j] % BIG_PRIME;
+    for (int j = 1; j < K; ++j) {
+        sum += nums[i][j] * j % BIG_PRIME;
     }
     return sum % BIG_PRIME;
 }
 
 bool same(int a, int b) {
-    // 雪花1开始坐标始终为0，雪花2开始坐标从0-5
-    for (int i = 0; i < 6; i++) {
-        // 正旋
-        for (int k = 0; k < 6; ++k) {
-            if (snows[a][k % 6] != snows[b][(i + k) % 6]) {
-                break;
-            }
-            if (k == 5) {
-                return true;
-            }
-        }
-        // 逆旋
-        for (int k = 0; k < 6; ++k) {
-            if (snows[a][k % 6] != snows[b][(i - k + 6) % 6]) {
-                break;
-            }
-            if (k == 5) {
-                return true;
-            }
+    for (int i = 1; i < K; ++i) {
+        if (nums[a][i] != nums[b][i]) {
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main() {
-    cin >> n;
-    memset(map, 0, sizeof(map));
-    bool find = false;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            scanf("%d", &snows[i][j]);
+    cin >> N >> K;
+    int res = 0;
+    memset(hashTable, -1, sizeof(hashTable));
+    for (int i = 1; i <= N; ++i) {
+        int num;
+        scanf("%d", &num);
+        for (int j = 0; j < K; ++j) {
+            sums[i][j] = sums[i - 1][j] + (num & 1);
+            nums[i][j] = sums[i][j] - sums[i][0];
+            num >>= 1;
         }
-        if (!find) {
-            int hash = getHash(i);
-            int k = 0;
-            while (map[hash][k]) {
-                if (same(i, map[hash][k])) {
-                    find = true;
-                    break;
-                }
-                k++;
+        hashTable[0][0] = 0;
+        int hash = getHash(i);
+        int k = 0;
+        while (hashTable[hash][k] != -1) {
+            if (same(i, hashTable[hash][k])) {
+                res = max(res, i - hashTable[hash][k]);
+                break;
             }
-            map[hash][k] = i;
+            k++;
+        }
+        if (hashTable[hash][k] == -1) {
+            hashTable[hash][k] = i;
         }
     }
-    if (find) {
-        cout << "Twin snowflakes found." << endl;
-    } else {
-        cout << "No two snowflakes are alike." << endl;
-    }
-
+    printf("%d\n", res);
+    return 0;
 }

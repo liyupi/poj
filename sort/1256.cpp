6e3fee92801d05c3f9ac510f014953ffdba65ebb
Yupi Li <592789970@qq.com>
2019-05-01 10:20:49 +0800

    update

--- sort/1256.cpp
+++ sort/1256.cpp
@@ -9,16 +9,26 @@ const int MAXN = 15;
 
 char word[MAXN];
 
+bool cmp(const char &a, const char &b) {
+    if ((a >= 97 && b >= 97) || (a < 97 && b < 97)) {
+        return a < b;
+    } else if (a >= 97) {
+        return a - 32 < b;
+    } else if (b >= 97) {
+        return a <= b - 32;
+    }
+}
+
 int main() {
     int t;
     cin >> t;
     while (t--) {
         scanf("%s", word);
         int len = strlen(word);
-        sort(word, word + len);
+        sort(word, word + len, cmp);
         do {
             printf("%s\n", word);
-        } while (next_permutation(word, word + len));
+        } while (next_permutation(word, word + len, cmp));
     }
 }
 
